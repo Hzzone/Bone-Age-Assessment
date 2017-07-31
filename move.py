397c779c237cf9ef66c61309781a69eaa35c076b
Hzzone <Hzzone@outlook.com>
2017-07-31 21:52:59 +0800

    preprocess and move

---
+++ move.py
@@ -0,0 +1,41 @@
+import dicom
+import os
+import datetime
+import shutil
+# for root, dirs, files in os.walk("/Volumes/Hzzone-Disk/18"):
+#     for file in files:
+#         if os.path.isdir(os.path.join(root, file)):
+#             continue
+#         ds = dicom.read_file(os.path.join(root, file))
+#         birthDate = ds.PatientBirthDate
+#         studyDate = ds.StudyDate
+#         bDate = birthDate[0:4] + '-' + birthDate[4:6] + '-' + birthDate[6:8]
+#         sDate = studyDate[0:4] + '-' + studyDate[4:6] + '-' + studyDate[6:8]
+#         bd = datetime.datetime.strptime(bDate, '%Y-%m-%d')
+#         sd = datetime.datetime.strptime(sDate, '%Y-%m-%d')
+#         days = (sd - bd).days
+#         age = int(round(days / 365))
+#         print(age)
+#         # shutil.move(os.path.join(root, file), "/Volumes/Hzzone-Disk/"+str(age))
+
+def move(source):
+    for root, dirs, files in os.walk(source):
+        for file in files:
+            shutil.move(os.path.join(root, file), source)
+        # for d in dirs:
+        #     os.remove(os.path.join(root, d))
+            # print(os.path.join(root, d))
+
+def rename(source):
+    index = 0
+    for root, dirs, files in os.walk(source):
+        for file in files:
+            index = index + 1
+            os.rename(os.path.join(root, file), os.path.join(root, str(index)+".jpg"))
+            # print(os.path.join(root, str(index)+".jpg"))
+            # print(len(files))
+
+if __name__ == "__main__":
+    # rename("/Users/HZzone/Desktop/image")
+    move("/Users/HZzone/Desktop/image/20")
+
---
+++ preprocess.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+
+
+def resize(source):
+    img = Image.open(source)
+    w, h = img.size
+    x1=0
+    x2=0
+    y1=0
+    y2=0
+    if w > h:
+        x1 = (w-h)/2
+        x2 = (w+h)/2
+        y1 = 0
+        y2 = h
+    else:
+        x1 = 0
+        x2 = w
+        y1 = (h-w)/2
+        y2 = (h+w)/2
+    box = (x1, y1, x2, y2)
+    img = img.crop(box)
+    img = img.resize((227, 227), Image.ANTIALIAS)
+    img.save(source)
+
+
+if __name__ == "__main__":
+    # resize("/Users/HZzone/Desktop/280.jpg")
+    for root, dirs, files in os.walk("/Users/HZzone/Desktop/image"):
+        for file in files:
+            path = os.path.join(root, file)
+            print(path)
+            resize(path)
