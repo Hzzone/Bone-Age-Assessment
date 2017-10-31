8c898fbf6dfcc9dce2b8611647c8288af873a185
Hzzone <Hzzone@outlook.com>
2017-10-31 13:35:32 +0800

    update

--- python/predict.py
+++ python/predict.py
@@ -49,15 +49,17 @@ def predict_dir(caffemodel, deploy, source_list, IMAGE_SIZE=227, LAYER_NAME="my-
     else:
         caffe.set_mode_cpu()
     net = caffe.Net(deploy, caffemodel, caffe.TEST)
-    # net.blobs['data'].reshape(len(file_list), 3, IMAGE_SIZE, IMAGE_SIZE)
-    # net.blobs['data'].data[...] = images
-    net.blobs['data'].reshape(len(file_list)/2, 3, IMAGE_SIZE, IMAGE_SIZE)
-    net.blobs['data'].data[...] = images[:len(file_list)/2]
-    output1 = net.forward()
-    net.blobs['data'].reshape(len(file_list) - len(file_list)/2, 3, IMAGE_SIZE, IMAGE_SIZE)
-    net.blobs['data'].data[...] = images[len(file_list)/2:]
-    output2 = net.forward()
-    output = dict(output1.items()+output2.items())
+    output = {}
+    for x in range(0, len(file_list), 10):
+        # net.blobs['data'].reshape(100, 3, IMAGE_SIZE, IMAGE_SIZE)
+        if len(file_list) - x < 10:
+            net.blobs['data'].reshape(len(file_list) - x, 3, IMAGE_SIZE, IMAGE_SIZE)
+            net.blobs['data'].data[...] = images[x:]
+        else:
+            net.blobs['data'].reshape(10, 3, IMAGE_SIZE, IMAGE_SIZE)
+            net.blobs['data'].data[...] = images[x:x+10]
+        o = net.forward()
+        output = dict(output.items()+o.items())
     for index, result in enumerate(output[LAYER_NAME]):
         predict_age = result[0]
         real_age = real_ages[index]
@@ -167,12 +169,13 @@ def predict_dir_output_temp(caffemodel, deploy, source, mode=True, IMAGE_SIZE=22
         # print(key, value)
     f.close()
 
-def predict_dir_output_temp_1(caffemodel, deploy, source, mode=True, IMAGE_SIZE=227, LAYER_NAME="my-fc8", LOGFILE="predict.log", BORDER_AGE=18):
+def predict_dir_output_temp_1(caffemodel, deploy, source_list, mode=True, IMAGE_SIZE=227, LAYER_NAME="my-fc8", LOGFILE="predict.log", BORDER_AGE=18):
     # f = open("predict.log", "w")
     file_list = []
-    for root, dirs, files in os.walk(source):
-        for file in files:
-            file_list.append(os.path.join(root, file))
+    for source in source_list:
+        for root, dirs, files in os.walk(source):
+            for file in files:
+                file_list.append(os.path.join(root, file))
     images = np.zeros((len(file_list), 3, IMAGE_SIZE, IMAGE_SIZE), dtype=np.float)
 
     # read age list
@@ -180,7 +183,7 @@ def predict_dir_output_temp_1(caffemodel, deploy, source, mode=True, IMAGE_SIZE=
     for index, dicom_file in enumerate(file_list):
         real_age = info.getInfo(dicom_file)
         real_ages.append(real_age)
-        images[index, :, :, :] = preprocess.process(dicom_file)
+        images[index, :, :, :] = preprocess.process(dicom_file, IMAGE_SIZE=IMAGE_SIZE)
     if mode:
         caffe.set_mode_gpu()
     else:
@@ -195,11 +198,14 @@ def predict_dir_output_temp_1(caffemodel, deploy, source, mode=True, IMAGE_SIZE=
         predict_age = result[0]
         real_age = real_ages[index]
         file_name = file_list[index]
-        id = dicom.read_file(file_list[index]).PatientID
+        ds = dicom.read_file(file_list[index])
+        id = ds.PatientID
+        birthDate = ds.PatientBirthDate
+        studyDate = ds.StudyDate
         # only see the error sample
         # if (predict_age > BORDER_AGE and real_age > BORDER_AGE) or (predict_age <= BORDER_AGE and real_age <= BORDER_AGE):
         #     continue
-        line = "%s %s %s %s %s" % (file_name.split('/')[-1], id, real_age, predict_age, abs(predict_age-real_age))
+        line = "%s %s %s %s %s %s %s" % (file_name.split('/')[-1], id, birthDate, studyDate, real_age, predict_age, abs(predict_age-real_age))
         dic[line] = abs(predict_age - real_age)
     dic = sorted(dic.items(), key=lambda d: d[1], reverse=True)
     for key, value in dic:
@@ -219,11 +225,11 @@ def predict_dir_output_temp_1(caffemodel, deploy, source, mode=True, IMAGE_SIZE=
 # # #####
 # predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/bysex/female/googlenet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/female/GoogLeNet/googlenet_deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1/female", "/home/hzzone/Bone-Age-Data/test2/female"], LOG_FILE="googlenet_female_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-loss3/classifier")
 # predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/bysex/male/googlenet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/male/GoogLeNet/googlenet_deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1/male", "/home/hzzone/Bone-Age-Data/test2/male"], LOG_FILE="googlenet_male_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-loss3/classifier")
-predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/all/googlenet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/all/GoogLeNet/googlenet_deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1", "/home/hzzone/Bone-Age-Data/test2"], LOG_FILE="googlenet_all_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-loss3/classifier")
+# predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/all/googlenet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/all/GoogLeNet/googlenet_deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1", "/home/hzzone/Bone-Age-Data/test2"], LOG_FILE="googlenet_all_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-loss3/classifier")
 # #####
-predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/bysex/female/resnet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/female/ResNet/resnet_deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1/female", "/home/hzzone/Bone-Age-Data/test2/female"], LOG_FILE="resnet_female_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-fc1000")
-predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/bysex/male/resnet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/male/ResNet/resnet_deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1/male", "/home/hzzone/Bone-Age-Data/test2/male"], LOG_FILE="resnet_male_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-fc1000")
-predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/all/resnet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/all/ResNet/resnet_deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1", "/home/hzzone/Bone-Age-Data/test2"], LOG_FILE="resnet_all_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-fc1000")
+# predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/bysex/female/resnet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/female/ResNet-50/deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1/female", "/home/hzzone/Bone-Age-Data/test2/female"], LOG_FILE="resnet_female_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-fc1000")
+# predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/bysex/male/resnet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/male/ResNet-50/deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1/male", "/home/hzzone/Bone-Age-Data/test2/male"], LOG_FILE="resnet_male_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-fc1000")
+# predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/all/resnet", test_deploy="/home/hzzone/Bone-Age-Assessment/train/all/ResNet-50/deploy.prototxt", test_data_source_list=["/home/hzzone/Bone-Age-Data/test1", "/home/hzzone/Bone-Age-Data/test2"], LOG_FILE="resnet_all_predict.log", IMAGE_SIZE=224, LAYER_NAME="my-fc1000")
 # predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/male", test_deploy="/home/hzzone/Bone-Age-Assessment/CaffeNet/deploy.prototxt", test_data_source="/home/hzzone/temp/male", LOG_FILE="male_predict.log")
 # predict_dir_output_temp(caffemodel="/home/hzzone/1tb/bone-age-model/female/caffenet_train_iter_1200.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/CaffeNet/deploy.prototxt", source="/home/hzzone/temp/female", LOGFILE="female_error.log")
 # predict_dir_output_temp(caffemodel="/home/hzzone/1tb/bone-age-model/male/caffenet_train_iter_2500.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/CaffeNet/deploy.prototxt", source="/home/hzzone/temp/male", LOGFILE="male_error.log")
@@ -234,3 +240,15 @@ predict_by_caffemodel_dir(caffemodel_source="/home/hzzone/1tb/bone-age-model/all
 
 # print predict_dir(caffemodel="/home/hzzone/1tb/bone-age-model/female/caffenet_train_iter_1200.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/CaffeNet/deploy.prototxt", source="/home/hzzone/processed/female/test", IMAGE_SIZE=227, LAYER_NAME="my-fc8", mode=True, BORDER_AGE=18)
 # print predict_dir(caffemodel="/home/hzzone/1tb/bone-age-model/male/caffenet_train_iter_2500.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/CaffeNet/deploy.prototxt", source="/home/hzzone/processed/male/test", IMAGE_SIZE=227, LAYER_NAME="my-fc8", mode=True, BORDER_AGE=18)
+
+predict_dir_output_temp_1(caffemodel="/home/hzzone/1tb/bone-age-model/bysex/male/alexnet/male_alexnet_train_iter_800.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/train/male/AlexNet/alexnet_deploy.prototxt", source_list=["/home/hzzone/Bone-Age-Data/test1/male"], LOGFILE="male_test1.log", IMAGE_SIZE=227)
+
+predict_dir_output_temp_1(caffemodel="/home/hzzone/1tb/bone-age-model/bysex/male/alexnet/male_alexnet_train_iter_800.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/train/male/AlexNet/alexnet_deploy.prototxt", source_list=["/home/hzzone/Bone-Age-Data/test2/male"], LOGFILE="male_test2.log", IMAGE_SIZE=227)
+
+predict_dir_output_temp_1(caffemodel="/home/hzzone/1tb/bone-age-model/bysex/female/caffenet/female_caffenet_train_iter_3900.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/train/female/CaffeNet/caffenet_deploy.prototxt", source_list=["/home/hzzone/Bone-Age-Data/test1/female"], LOGFILE="female_test1.log", IMAGE_SIZE=227)
+
+predict_dir_output_temp_1(caffemodel="/home/hzzone/1tb/bone-age-model/bysex/female/caffenet/female_caffenet_train_iter_3900.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/train/female/CaffeNet/caffenet_deploy.prototxt", source_list=["/home/hzzone/Bone-Age-Data/test2/female"], LOGFILE="female_test2.log", IMAGE_SIZE=227)
+
+predict_dir_output_temp_1(caffemodel="/home/hzzone/1tb/bone-age-model/all/caffenet/all_caffenet_train_iter_4000.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/train/male/CaffeNet/caffenet_deploy.prototxt", source_list=["/home/hzzone/Bone-Age-Data/test1"], LOGFILE="all_test1.log", IMAGE_SIZE=227)
+
+predict_dir_output_temp_1(caffemodel="/home/hzzone/1tb/bone-age-model/all/caffenet/all_caffenet_train_iter_4000.caffemodel", deploy="/home/hzzone/Bone-Age-Assessment/train/male/CaffeNet/caffenet_deploy.prototxt", source_list=["/home/hzzone/Bone-Age-Data/test2"], LOGFILE="all_test2.log", IMAGE_SIZE=227)
--- python/visualize.py
+++ python/visualize.py
@@ -1,6 +1,6 @@
 import sys
 # sys.path.insert(0, "/home/bw/code/caffe/python")
-sys.path.insert(0, "/Users/HZzone/caffe/python")
+sys.path.insert(0, "/home/hzzone/caffe/python")
 import caffe
 import numpy as np
 import os
@@ -11,7 +11,7 @@ from scipy.misc import imsave
 import dicom
 
 # predict the age from a new dicom file by a trained caffemodel and deploy file
-def visualize_layers(caffemodel, deploy, dicom_file, IMAGE_SIZE=227, mode=True):
+def visualize_layers(caffemodel, deploy, dicom_file, IMAGE_SIZE=227, mode=True, save_dir=""):
     im = preprocess.process(dicom_file, IMAGE_SIZE=IMAGE_SIZE)
     patientID = dicom.read_file(dicom_file).PatientID
     if mode:
@@ -23,7 +23,7 @@ def visualize_layers(caffemodel, deploy, dicom_file, IMAGE_SIZE=227, mode=True):
     # read a dicom file
     net.blobs['data'].data[...] = im
     net.forward()
-    curr_path = os.path.dirname(os.path.abspath(__file__))
+    # curr_path = os.path.dirname(os.path.abspath(__file__))
     # get every layer feature map and save to files
     for layer_name, param in net.params.iteritems():
             try:
@@ -31,14 +31,14 @@ def visualize_layers(caffemodel, deploy, dicom_file, IMAGE_SIZE=227, mode=True):
             except:
                 continue
 
-            features_path = os.path.join(curr_path, patientID)
+            features_path = os.path.join(save_dir, patientID)
             # if os.path.exists(features_path):
             #     shutil.rmtree(features_path)
             if not os.path.exists(features_path):
                 os.mkdir(features_path)
             features_path = os.path.join(features_path, layer_name)
-            # if os.path.exists(features_path):
-            #     shutil.rmtree(features_path)
+            if os.path.exists(features_path):
+                shutil.rmtree(features_path)
             os.mkdir(features_path)
 
             try:
@@ -89,14 +89,33 @@ def visualize_output_size(caffemodel, deploy, dicom_file, IMAGE_SIZE=227):
     for layer_name, param in net.params.iteritems():
         print layer_name + '\t' + str(param[0].data.shape), str(param[1].data.shape)
 
+def save_dir_feature_map(source_list, save_dir, caffemodel, deploy_file):
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    file_list = []
+    for source in source_list:
+        for root, dirs, files in os.walk(source):
+            for dicom_file in files:
+                file_list.append(os.path.join(root, dicom_file))
+    for dicom_file in file_list:
+        visualize_layers(dicom_file=dicom_file, caffemodel=caffemodel, deploy=deploy_file, save_dir=save_dir)
+
 if __name__=="__main__":
-    for root, dirs, files in os.walk("/Volumes/Hzzone-disk/DeepLearning/male_regression/test"):
-        for index, dicom_file in enumerate(files):
-            visualize_layers("/Volumes/Hzzone-disk/DeepLearning/male_regression/CaffeNet/model/caffenet_train_iter_2000.caffemodel",
-                             "/Volumes/Hzzone-disk/DeepLearning/male_regression/CaffeNet/deploy.prototxt",
-                             os.path.join(root, dicom_file), mode=False)
+    # for root, dirs, files in os.walk("/Volumes/Hzzone-disk/DeepLearning/male_regression/test"):
+    #     for index, dicom_file in enumerate(files):
+    #         visualize_layers("/Volumes/Hzzone-disk/DeepLearning/male_regression/CaffeNet/model/caffenet_train_iter_2000.caffemodel",
+    #                          "/Volumes/Hzzone-disk/DeepLearning/male_regression/CaffeNet/deploy.prototxt",
+    #                          os.path.join(root, dicom_file), mode=False)
     # for root, dirs, files in os.walk("/Volumes/Hzzone-disk/DeepLearning/female_regression/test"):
     #     for index, dicom_file in enumerate(files):
     #         visualize_layers("/Volumes/Hzzone-disk/DeepLearning/female_regression/CaffeNet/model/caffenet_train_iter_2000.caffemodel",
     #                          "/Volumes/Hzzone-disk/DeepLearning/female_regression/CaffeNet/deploy.prototxt",
     #                          os.path.join(root, dicom_file), mode=False)
+    save_dir_feature_map(save_dir="/home/hzzone/features/male_test1", source_list=["/home/hzzone/Bone-Age-Data/test1/male"], caffemodel="/home/hzzone/1tb/bone-age-model/bysex/male/alexnet/male_alexnet_train_iter_800.caffemodel", deploy_file="/home/hzzone/Bone-Age-Assessment/train/male/AlexNet/alexnet_deploy.prototxt")
+    save_dir_feature_map(save_dir="/home/hzzone/features/male_test2", source_list=["/home/hzzone/Bone-Age-Data/test2/male"], caffemodel="/home/hzzone/1tb/bone-age-model/bysex/male/alexnet/male_alexnet_train_iter_800.caffemodel", deploy_file="/home/hzzone/Bone-Age-Assessment/train/male/AlexNet/alexnet_deploy.prototxt")
+
+    save_dir_feature_map(save_dir="/home/hzzone/features/female_test1", source_list=["/home/hzzone/Bone-Age-Data/test1/female"], caffemodel="/home/hzzone/1tb/bone-age-model/bysex/female/caffenet/female_caffenet_train_iter_3900.caffemodel", deploy_file="/home/hzzone/Bone-Age-Assessment/train/female/CaffeNet/caffenet_deploy.prototxt")
+    save_dir_feature_map(save_dir="/home/hzzone/features/female_test2", source_list=["/home/hzzone/Bone-Age-Data/test2/female"], caffemodel="/home/hzzone/1tb/bone-age-model/bysex/female/caffenet/female_caffenet_train_iter_3900.caffemodel", deploy_file="/home/hzzone/Bone-Age-Assessment/train/female/CaffeNet/caffenet_deploy.prototxt")
+
+    save_dir_feature_map(save_dir="/home/hzzone/features/all_test1", source_list=["/home/hzzone/Bone-Age-Data/test1/female", "/home/hzzone/Bone-Age-Data/test1/male"], caffemodel="/home/hzzone/1tb/bone-age-model/all/caffenet/all_caffenet_train_iter_4000.caffemodel", deploy_file="/home/hzzone/Bone-Age-Assessment/train/male/CaffeNet/caffenet_deploy.prototxt")
+    save_dir_feature_map(save_dir="/home/hzzone/features/all_test2", source_list=["/home/hzzone/Bone-Age-Data/test2/female", "/home/hzzone/Bone-Age-Data/test2/male"], caffemodel="/home/hzzone/1tb/bone-age-model/all/caffenet/all_caffenet_train_iter_4000.caffemodel", deploy_file="/home/hzzone/Bone-Age-Assessment/train/female/CaffeNet/caffenet_deploy.prototxt")
